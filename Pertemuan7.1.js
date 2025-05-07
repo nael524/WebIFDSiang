d4cfea5c80c658c2df8e28978ce257541f7d9cd3
nael524 <2403310164@students.satyaterrabhinneka.ac.id>
2025-05-07 22:37:27 +0700

    Pertemuan7 No 1

---
+++ Pertemuan7.1.js
@@ -0,0 +1,11 @@
+let firstNama = "Jonee";
+let middlane = "Doee \n";
+let old = 2009;
+let oldnow = 2025;
+let now = oldnow - old;
+let text = "Helloo...\n";
+let text1 = "You are  ";
+let text2 = "Years old";
+const hasil =
+  text + firstNama + " " + middlane + text1 + "" + now + " " + text2;
+console.log(hasil);
