57cf3c2b0d5f96865d7f4f3bd8a618cb664ecaa3
nael524 <2403310164@students.satyaterrabhinneka.ac.id>
2025-05-07 22:38:32 +0700

    Pertemuan7 No 3

---
+++ Pertemuan7.3.js
@@ -0,0 +1,9 @@
+let panjang = 20;
+let lebar = 5;
+let text = "Halooo Kita Akan Menyelesaikan Rumus Persegi Panjang";
+let luas = panjang * lebar;
+let keliling = 2 * (panjang + lebar);
+console.log(text);
+console.log("Diketahui:" + "\nPanjang:" + panjang + "\nLebar:" + lebar);
+console.log("Luas Persegi Panjang Adalah: " + luas);
+console.log("Keliling Persegi Panjang Adalah: " + keliling);
